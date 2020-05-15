bd4a64420ef82d858d5d45cf2e74f7ae492be5df
Yago Santos de Sousa <yagosansz@gmail.com>
2020-05-14 21:59:38 -0400

    Updated routes, repositories, and services

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce<Balance>(
+      (acc: Balance, curr: Transaction) => {
+        if (curr.type === 'income') {
+          acc.income += curr.value;
+          acc.total += curr.value;
+        } else {
+          acc.outcome += curr.value;
+          acc.total -= curr.value;
+        }
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
